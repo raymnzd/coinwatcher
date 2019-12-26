847b845db92a6dce27d70d73ef87394899dab5f9
raymond zhao <raymnzd@gmail.com>
2019-12-26 01:51:49 -0500

    displaying currencies by market cap rank

--- display/views.py
+++ display/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render
 from .cryptodata import CryptoData
+from operator import *
 
 def home(request):
     crypto_data = CryptoData()
 
     c_list = crypto_data.get_currencies()
     currencies = dict()
-    for i in range(5):
+    for i in range(25):
         currencies[c_list[i]['id']] = {
             'name' : c_list[i]['name'],
             'market_cap' : c_list[i]['market_cap'],
             'total_volume' : c_list[i]['total_volume'],
-            'current_price' : c_list[i]['current_price']
+            'current_price' : c_list[i]['current_price'],
+            'market_cap_rank': c_list[i]['market_cap_rank']
         }
+
+	#sort currencies by market cap rank
+    currencies = dict(sorted(currencies.items(),key=lambda x:getitem(x[1],'market_cap_rank')))
     context = {
-        #how many to show
-        'range' : range(5),
         'currencies' : currencies
     }
     return render(request, 'display/index.html', context)
